81a2f6ebf9afa0145991bbc6cb88266ceca515f1
Nicolas Muller <nicolas.j.muller@outlook.com>
2026-05-30 03:37:20 +0200

    Glitch
    Arriver de la page js et css qui s'occupent ensemble du glitch. Pour activer et desactivé, il suffit de modifier la constante "modeDegradé" de true à false.

---
+++ js/tempeteSolaire.js
@@ -0,0 +1,16 @@
+// true = activé l'effet tempete solaire / false = désactivé l'effet tempete solaire
+const modeDegradé = true;
+
+if (modeDegradé) {
+    // Ajoute la classe sur le body
+    document.body.classList.add('mode-degrade');
+
+    // Injecte le bandeau en haut de page
+    const bandeau = document.createElement('div');
+    bandeau.classList.add('bandeau-alerte');
+    bandeau.textContent = 'ALERTE !!! Tempête solaire détectée. Communications dégradées. Signal instable !!!';
+    document.body.prepend(bandeau);
+
+    // Décale le contenu pour ne pas être caché par le bandeau
+    document.body.style.paddingTop = '40px';
+}
